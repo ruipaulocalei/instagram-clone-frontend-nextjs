f20bc9d7b5041ec8f717ee367fa6fee832ee31f4
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-11-14 14:48:26 +0100

    Sidebar Component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,13 @@
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
     <div className='flex-col flex h-screen'>
       <Header />
-      <div className="w-full my-6 max-w-screen-xl mx-auto px-6"></div>
+      <div className="w-full my-6 max-w-screen-xl mx-auto px-6">
+        <Sidebar />
+      </div>
     </div>
   )
 }
