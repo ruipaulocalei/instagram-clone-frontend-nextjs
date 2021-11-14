99d25b256fe5da77ab86878f325f610447ee9d45
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-11-14 13:01:17 +0100

    Input Component

---
+++ components/Input.tsx
@@ -0,0 +1,7 @@
+import React, { InputHTMLAttributes } from 'react'
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>
+export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  return <input className="w-full p-3 focus:outline-none 
+  border-2 focus:border-pink-600 rounded-md" {...props} ref={ref} />;
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Button from '../components/Button'
+import { Input } from '../components/Input'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -9,11 +10,9 @@ const Home: NextPage = () => {
     <div className='flex items-center h-screen w-screen justify-center'>
       <form className='flex flex-col w-full max-w-sm p-8 
       bg-gray-900 space-y-4 rounded-lg'>
-        <input type="text" className='w-full p-3 focus:outline-none 
-        border-2 focus:border-pink-600 rounded-md' />
-        <input type="text" className='w-full p-3 focus:outline-none 
-        border-2 focus:border-pink-600 rounded-md' />
-        <Button loading={true} buttonName={'Entrar'} isClicable={false} />
+        <Input />
+        <Input />
+        <Button loading={false} buttonName={'Entrar'} isClicable={true} />
       </form>
     </div>
   )
