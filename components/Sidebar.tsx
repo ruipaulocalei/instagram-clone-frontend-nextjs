c591b527bbd981db78e26f2691df41bcd2d5e913
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-11-16 08:57:21 +0100

    Dashboard

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,17 +1,19 @@
 import { MenuIcon, UserGroupIcon } from '@heroicons/react/outline'
 export function Sidebar() {
   return (
-    <aside className='w-64 mr-8'>
-      <div>
-        <h1 className='font-bold text-gray-400 text-xl uppercase'>Geral</h1>
-        <div className='items-stretch mt-8 space-y-2'>
-          <div className='flex items-center space-x-6'>
-            <MenuIcon className='h-5 w-5' />
-            <h3 className='font-medium'>Dashboard</h3>
-          </div>
-          <div className='flex items-center space-x-6'>
-            <UserGroupIcon className='h-5 w-5' />
-            <h3 className='font-medium'>Usuários</h3>
+    <aside className='w-48 mr-8'>
+      <div className='flex'>
+        <div>
+          <h1 className='font-bold text-gray-400 text-xl uppercase'>Geral</h1>
+          <div className='items-stretch mt-8 space-y-2'>
+            <div className='flex items-center space-x-6'>
+              <MenuIcon className='h-5 w-5' />
+              <h3 className='font-medium'>Dashboard</h3>
+            </div>
+            <div className='flex items-center space-x-6'>
+              <UserGroupIcon className='h-5 w-5' />
+              <h3 className='font-medium'>Usuários</h3>
+            </div>
           </div>
         </div>
       </div>
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,59 @@
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import dynamic from 'next/dynamic'
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    },
+  },
+  grid: {
+    show: false
+  },
+  tooltip: {
+    enabled: false
+  },
+  // xaxis: {
+  //   type: 'datetime'
+  // },
+  // categories: [
+  //   '2021-11-14T01:00:00.000z',
+  //   '2021-11-15T01:00:00.000z',
+  //   '2021-11-16T01:00:00.000z',
+  //   '2021-11-17T01:00:00.000z',
+  //   '2021-11-18T01:00:00.000z',
+  //   '2021-11-19T01:00:00.000z',
+  // ]
+}
+const series = [
+  {
+    name: 'serie1', data: [43, 90, 23, 9, 21, 23]
+  }
+]
 
 export default function Dashboard() {
   return (
     <div className='flex-col flex h-screen'>
       <Header />
-      <div className="w-full my-6 max-w-screen-xl mx-auto px-6">
+      <div className="w-full my-6 mx-auto px-6 space-y-6 sm:flex">
         <Sidebar />
+        <aside className="grid gap-4 md:grid-cols-1 lg:grid-cols-2 flex-1 items-start">
+          <div className="bg-red-400 p-8 rounded-lg">
+            <Chart options={options} type='area' height={160} series={series} />
+            <p>Inscritos da semana</p>
+          </div>
+          <div className="bg-red-400 p-8 rounded-lg">
+            <p>Taxa de abertura</p>
+          </div>
+        </aside>
       </div>
     </div>
   )
